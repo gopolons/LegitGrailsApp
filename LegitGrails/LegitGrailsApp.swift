66f64e20a3b18a7ff0c8eb72f6f5e0bc0b8fb1b6
Georgii Polonskii <gopolons@gmail.com>
2021-11-29 15:48:18 +0000

    Initial Commit

---
+++ LegitGrails/LegitGrailsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LegitGrailsApp.swift
+//  LegitGrails
+//
+//  Created by Georgy Polonskiy on 29/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct LegitGrailsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
